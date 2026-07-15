fbd01b34b4b5db2bdc319ee310dab922451db92a
M. Ali Sher <sans42699@gmail.com>
2026-07-15 14:52:47 +0500

    Blurring successfully simulated

--- Blur_Data.py
+++ Blur_Data.py
@@ -12,15 +12,17 @@ sigmaG = 2 # for gaussian noise
 
 ignore_val = 0
 
-sigmaG_min = 1
+sigmaG_min = 0.5
 sigmaG_max = 3
 keep_fraction_either_side_min=0.3
 keep_fraction_either_side_max=0.6
-max_shift_min = 0.1
-max_shift_max = 0.3
-sigma_min = 0.02 # for rican noise
-sigma_max = 0.05
+max_shift_min = 0.05
+max_shift_max = 0.35
+sigma_min = 0.03 # for rican noise
+sigma_max = 0.09
+
 save_as_volume = 0
+save_as_slice = 0
 save_images = 0
 
 def save_as_png(array, filename):
@@ -58,27 +60,34 @@ def pkload(fname):
 folder1 = "/home/jarvis/Documents/MRI Data/IXI_data/IXI_data/Test/"
 folder2 = "/home/jarvis/Documents/MRI Data/IXI_data/IXI_data/Train/"
 folder3 = "/home/jarvis/Documents/MRI Data/IXI_data/IXI_data/Val/"
-save_as_volume = int(input("\nSave files as entire volume(1 for yes, 0 for no) - Press 0 if crashes are frequent : "))
-if save_as_volume == 0:
-    save_images = int(input("\nSave files as images too(1 for yes, 0 for no) : "))
+folderList = [folder1,folder2,folder3]
+
+save_as_volume = int(input("\nSave files as entire volumes(1 for yes, 0 for no) : "))
+save_as_slice = int(input("\nSave files as slices(1 for yes, 0 for no) : "))
+save_images = int(input("\nSave files as images(1 for yes, 0 for no) : "))
+
 print("\n")
-folderList = [folder1]#,folder2,folder3]
 if __name__ == "__main__":
     for folderName in folderList:
         for filename in os.listdir(folderName):
-            VoxelCluster,ignore_val = pkload(folderName +"/"+ filename)
             name, ext = os.path.splitext(filename)
             if ext!=".pkl":
                 continue
+            VoxelCluster,ignore_val = pkload(folderName +"/"+ filename)
+            tamperedVoxelCluster = VoxelCluster.copy()
             slice_index = 0
+            if save_as_volume == 1:    
+                np.save(folderName +"/" + name + ".npy", VoxelCluster)
             for slice in VoxelCluster:
-                if save_as_volume == 0 and save_images == 1:
+                if save_images == 1:
                     save_as_png(slice,folderName + "/" + name +"_"+ str(slice_index) + ".png")
+                if save_as_slice == 1:    
+                    np.save(folderName +"/" + name +"_"+ str(slice_index)+ ".npy", slice)
 
                 max_shift = random.uniform(max_shift_min, max_shift_max)
                 keep_fraction_either_side = random.uniform(keep_fraction_either_side_min,keep_fraction_either_side_max)
                 sigma = random.uniform(sigma_min,sigma_max)
-                sigmaG = random.uniform(sigmaG_min,sigmaG_max)                
+                sigmaG = random.uniform(sigmaG_min,sigmaG_max)              
 
                 tamperedSlice = gaussian_filter(slice,sigmaG)
                 k_space = np.fft.fftshift(np.fft.fft2(tamperedSlice))
@@ -87,13 +96,13 @@ if __name__ == "__main__":
                 tamperedSlice = np.abs(np.fft.ifft2(np.fft.ifftshift(k_space)))
                 tamperedSlice = add_rician_noise(tamperedSlice)
 
-                VoxelCluster[slice_index] = tamperedSlice
+                tamperedVoxelCluster[slice_index] = tamperedSlice
 
-                if save_as_volume == 0:
+                if save_as_slice == 1:
                     np.save(folderName + "/" + name + "_blurred_"+ str(slice_index) + ".npy", tamperedSlice)
-                    if save_images == 1:
-                        save_as_png(tamperedSlice,folderName + "/" + name + "_blurred_"+ str(slice_index) + ".png")
+                if save_images == 1:
+                    save_as_png(tamperedSlice,folderName + "/" + name + "_blurred_"+ str(slice_index) + ".png")
 
                 slice_index+=1
             if save_as_volume == 1:    
-                np.save(folderName +"/blurred_" + name + ".npy", VoxelCluster)
+                np.save(folderName +"/blurred_" + name + ".npy", tamperedVoxelCluster)
